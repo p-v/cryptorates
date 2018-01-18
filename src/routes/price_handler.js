9615012cb16e3c31f82ff787a18964096c84e79a
Pratyush <pratyushverma@outlook.com>
2018-01-18 17:24:03 +0530

    Fix error messages

--- src/routes/price_handler.js
+++ src/routes/price_handler.js
@@ -12,14 +12,18 @@ function handleBinance(pair, res) {
   const pairSplit = pair.split('-');
   const url = BINANCE_API + pairSplit[1] + pairSplit[0];
   request(url, function(err, body){
-    const response = JSON.parse(body.body);
-    if (err) {
-      res.status(500).send('Something went wrong')
+    if (err || body.statusCode !== 200) {
+      res.status(500).json({ error: 'Something went wrong' })
     } else {
-      res.json({
-        pair,
-        lastPrice: response.price, 
-      });
+      try {
+        const response = JSON.parse(body.body);
+        res.json({
+          pair,
+          lastPrice: response.price, 
+        });
+      } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
+      }
     }
   });
 }
@@ -27,14 +31,18 @@ function handleBinance(pair, res) {
 function handleBittrex(pair, res) {
   const url = BITTREX_API + pair;
   request(url, function(err, body){
-    if (err) {
-      res.status(500).send('Something went wrong')
+    if (err || body.statusCode !== 200) {
+      res.status(500).json({ error: 'Something went wrong' })
     } else {
-      const response = JSON.parse(body.body);
-      res.json({
-        pair,
-        lastPrice: response.result.Last,
-      });
+      try {
+        const response = JSON.parse(body.body);
+        res.json({
+          pair,
+          lastPrice: response.result.Last,
+        });
+      } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
+      }
     }
   });
 }
@@ -43,14 +51,18 @@ function handleCryptopia(pair, res) {
   const pairSplit = pair.split('-');
   const url = CRYPTOPIA_API + pairSplit[1] + '_' + pairSplit[0];
   request(url, function(err, body){
-    if (err) {
-      res.status(500).send('Something went wrong')
+    if (err || body.statusCode !== 200) {
+      res.status(500).json({ error: 'Something went wrong' })
     } else {
-      const response = JSON.parse(body.body);
-      res.json({
-        pair,
-        lastPrice: response.Data.LastPrice,
-      });
+      try {
+        const response = JSON.parse(body.body);
+        res.json({
+          pair,
+          lastPrice: response.Data.LastPrice,
+        });
+      } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
+      }
     }
   });
 }
@@ -58,22 +70,15 @@ function handleCryptopia(pair, res) {
 
 module.exports = function(req, res) {
   const { pair, exchange } = req.params;
-  if (!exchange || !pair) {
-    res.status(500).send('Check params')
-  }
-  try {
-    switch (exchange) {
-      case BITTREX:
-        handleBittrex(pair, res);
-        break;
-      case BINANCE:
-        handleBinance(pair, res);
-        break;
-      case CRYPTOPIA:
-        handleCryptopia(pair, res);
-        break;
-    }
-  } catch (error) {
-    res.status(500).send('Something went wrong')
+  switch (exchange) {
+    case BITTREX:
+      handleBittrex(pair, res);
+      break;
+    case BINANCE:
+      handleBinance(pair, res);
+      break;
+    case CRYPTOPIA:
+      handleCryptopia(pair, res);
+      break;
   }
 }
