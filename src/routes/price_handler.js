85cc15fc9e9a2e5eada64427676e565672bad20a
Pratyush <pratyushverma@outlook.com>
2018-01-29 13:03:48 +0530

    Add gateio exchange

--- src/routes/price_handler.js
+++ src/routes/price_handler.js
@@ -4,11 +4,33 @@ const BITTREX_API = 'https://bittrex.com/api/v1.1/public/getticker?market=';
 const BINANCE_API = 'https://api.binance.com/api/v1/ticker/price?symbol=';
 const CRYPTOPIA_API = 'https://www.cryptopia.co.nz/api/GetMarket/';
 const HITBTC_API = 'https://api.hitbtc.com/api/2/public/ticker/';
+const GATEIO_API = 'http://data.gate.io/api2/1/ticker/';
 
 const BITTREX = 'bittrex';
 const BINANCE = 'binance';
 const CRYPTOPIA = 'cryptopia';
 const HITBTC = 'hitbtc';
+const GATEIO = 'gateio';
+
+function handleGateio(pair, res) {
+  const pairSplit = pair.split('-');
+  const url = GATEIO_API + pairSplit[1] + '_' + pairSplit[0];
+  request(url, function(err, body){
+    if (err || body.statusCode !== 200) {
+      res.status(500).json({ error: 'Something went wrong' })
+    } else {
+      try {
+        const response = JSON.parse(body.body);
+        res.json({
+          pair,
+          lastPrice: response.last, 
+        });
+      } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
+      }
+    }
+  });
+}
 
 function handleBinance(pair, res) {
   const pairSplit = pair.split('-');
@@ -105,6 +127,9 @@ module.exports = function(req, res) {
       break;
     case HITBTC:
       handleHitbtc(pair, res);
+      break;
+    case GATEIO:
+      handleGateio(pair, res);
       break;
   }
 }
